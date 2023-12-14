09cbd3a5f404ef6288deefbb10bbefc58a0015be
wupdp <kirillbond2017@gmail.com>
2023-12-14 18:26:05 +0300

    FIND_EXTENTION

--- homework_bash_1/find_extention.sh
+++ homework_bash_1/find_extention.sh
@@ -4,5 +4,15 @@ directory=$2
 extension=$3
 output_file=$1
 
+if [ ! -d "$directory" ]; then
+    echo "Ошибка: Каталог '$directory' не существует."
+    exit 1
+fi
+
+if [ ! -r "$directory" ]; then
+    echo "Ошибка: Нет доступа к чтению каталога '$directory'."
+    exit 1
+fi
+
 find "$directory" -type f -name "*.$extension" > "$output_file"
 echo "Имена файлов с расширением .$extension из каталога $directory сохранены в $output_file"
